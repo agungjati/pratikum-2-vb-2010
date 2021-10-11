ccd60e8dd2087115c11e353720078f58ce54daca
Agung <Agung@AGUNG-PC>
2021-10-11 13:43:15 +0700

    finish

--- Tugas/Form1.vb
+++ Tugas/Form1.vb
@@ -5,9 +5,15 @@
     End Sub
 
     Private Sub txtQty_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtQty.TextChanged
-        txtJumlah.Text = Val(txtQty.Text)
+        txtJumlah.Text = Val(txtQty.Text) * Val(txtHarga.Text)
+    End Sub
 
+    Private Sub txtHarga_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtHarga.TextChanged
+        txtJumlah.Text = Val(txtQty.Text) * Val(txtHarga.Text)
+    End Sub
 
-
+    Private Sub txtDiskon_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDiskon.TextChanged
+        Dim diskon As Double = Val(txtJumlah.Text) * Val(txtDiskon.Text) / 100
+        txtTotal.Text = Val(txtJumlah.Text) - diskon
     End Sub
 End Class
